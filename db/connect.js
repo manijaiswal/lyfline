d9d7a63fc0a679c376de832cf7d9c94024019144
Manish Kumar <mk6598951@gmail.com>
2018-10-08 00:54:31 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -5,7 +5,7 @@ const log4jsLogger      =   require('../loggers/log4js_module');
 
 var logger              =   log4jsLogger.getLogger('DataBase');
 
-//mongodb://<dbuser>:<dbpassword>@ds137812.mlab.com:37812/flight-server   mongodb://flight:Nitp123@ds137812.mlab.com:37812/flight-server
+
 //'mongodb://localhost:27017/'+configs.DB_NAME
 
 //mongodb://<dbuser>:<dbpassword>@ds139645.mlab.com:39645/vizack
