816fb287784fd5e0f922f07fd6f4c0295336afe7
JAIMAKALI <mk6598951@gmail.com>
2019-03-02 08:09:52 +0530

    mail

--- mail-gun/sendMail.js
+++ mail-gun/sendMail.js
@@ -5,7 +5,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'eduvysupervision@gmail.com',
-        pass: 'Manish@1234',
+        pass: 'Manish@123456',
         clientId: '490182794397-i958cpolm0vkbr7gf8iovbuoa4akk7s2.apps.googleusercontent.com', //94430039257-g72ffjk1auimb6g7g74i8046nld8jqn1.apps.googleusercontent.com
         clientSecret: 'WJQnKpJI0JiF9-NymqJI6msZ',
         refreshToken: '1/CsM4ORXQbm8Z4Fmpums905CCWwMV-OKC0vNH5pPAHL5nvC6J1i-QndxU26BR5bMn', //1/BBIkh1kZgIMyvaADyC71Bo09bIMt_7yAn4T9IfoZiCo
